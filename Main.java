3943dd0bdfb4e62b933c648ddadad99e387a9cdc
morrc3611 <morrc3611@wrdsb.ca>
2020-09-14 13:53:51 +0000

    does division and finds the remainder

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  *
  * @author 
@@ -9,7 +11,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+   Scanner input = new Scanner(System.in);
+    //asks user to input numbers
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
     
+    //finds the quotient without decimals
+    int quotient = num1 / num2;
+
+    //finds the remainder
+   int remainder = num1 % num2;
+   
+   // tells user the answer
+    System.out.println (num1 + "/" + num2 + " is " + quotient + " with a remainder of " + remainder);
   }
 }
